8e1f57c43b0982947c55d5b0125b4f9797625932
Jeay <jeay2001@gmail.com>
2024-09-13 18:16:13 +0200

    Classes Aangemaakt en Migration geprobeerd

---
+++ DAL/ContosoPizzaContext.cs
@@ -0,0 +1,23 @@
+﻿using EF_Learning.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Learning.DAL
+{
+    internal class ContosoPizzaContext : DbContext
+    {
+        public DbSet<Customer> Customer { get; set; } = null!;
+        public DbSet<Order> Order { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.; Initial Catalog=ContosoPizza;Integrated Security=true;TrustServerCertificate=True;");
+        }
+    }
+}
